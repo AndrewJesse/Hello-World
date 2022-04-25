0c6f787e0819eece00d798ed2d07fb565452bdd1
A.J. Marchese <aj.marchese@outlook.com>
2022-04-25 06:36:02 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,10 +1,11 @@
 package edu.pcc.cis.helloworld;
-
+import java.util.Date;
 /**
  * Created by A.J. Marchese on 4/25/2022
  */
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
