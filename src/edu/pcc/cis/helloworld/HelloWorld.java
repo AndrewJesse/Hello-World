a4cdfed5d9b6879abc300e192571016a6ae70d14
A.J. Marchese <aj.marchese@outlook.com>
2022-04-25 06:33:28 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by A.J. Marchese on 4/25/2022
+ */
 public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+    }
 }
